9ae4f5a8103506d77f372a7a0a269e50cf89630a
Jonas Sundberg <jsund@jsund.com>
2010-05-29 21:33:14 +0200

    Added negative testing to StringCchLengthA-t and fixed code formatting.

--- tests/StringCchLengthA-t.c
+++ tests/StringCchLengthA-t.c
@@ -5,13 +5,19 @@
 #include "tap/basic.h"
 
 int main(void){
-	plan(4);
+	plan(5);
 
 	size_t length;
-	ok(SUCCEEDED(StringCchLengthA("", 1, &length)), "Calculate length of empty string.");
-	is_int(0, length, "Value for length of empty string.");
-	ok(SUCCEEDED(StringCchLengthA("test\0test", 10, &length)), "Calculate length of string with embedded null character.");
-	is_int(4, length, "Value for length of string with embedded null character.");
+	ok(SUCCEEDED(StringCchLengthA("", 1, &length)),
+			"Calculate length of empty string.");
+	is_int(0, length,
+			"Value for length of empty string.");
+	ok(SUCCEEDED(StringCchLengthA("test\0test", 10, &length)),
+			"Calculate length of string with embedded null character.");
+	is_int(4, length,
+			"Value for length of string with embedded null character.");
+	ok(STRSAFE_E_INVALID_PARAMETER == StringCchLengthA("too long", 8, &length),
+			"Make sure error is thrown if null termination is not found.");
 
 	return 0;
 }

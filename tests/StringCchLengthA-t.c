b77f5624d634c894afdf7b630c616f9b645fe956
Jonas Sundberg <jsund@jsund.com>
2010-05-29 21:44:07 +0200

    Added additional test in StringCchLengthA-t.

--- tests/StringCchLengthA-t.c
+++ tests/StringCchLengthA-t.c
@@ -2,13 +2,17 @@
 #include "tap/basic.h"
 
 int main(void){
-	plan(5);
+	plan(7);
 
 	size_t length;
 	ok(SUCCEEDED(StringCchLengthA("", 1, &length)),
 			"Calculate length of empty string.");
 	is_int(0, length,
 			"Value for length of empty string.");
+	ok(SUCCEEDED(StringCchLengthA("test", 5, &length)),
+			"Calculate length of short string.");
+	is_int(4, length,
+			"Value for length of short string.");
 	ok(SUCCEEDED(StringCchLengthA("test\0test", 10, &length)),
 			"Calculate length of string with embedded null character.");
 	is_int(4, length,

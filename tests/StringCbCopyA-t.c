6587a1822751894fa49767457e0ea7eb222ea007
Jonas Sundberg <jsund@jsund.com>
2010-05-29 23:01:56 +0200

    Added tests for CopyA and CopyNA.

--- tests/StringCbCopyA-t.c
+++ tests/StringCbCopyA-t.c
@@ -2,7 +2,20 @@
 #include "tap/basic.h"
 
 int main(void){
-	plan(0);
+	plan(5);
+
+	char dest[11];
+	ok(SUCCEEDED(StringCbCopyA(dest, 11, "test")),
+			"Copy short string.");
+	is_string("test", dest,
+			"Result of copying short string.");
+	ok(StringCbCopyA(dest, 11, "longer string") ==
+			STRSAFE_E_INSUFFICIENT_BUFFER,
+			"Copy a string that is too long.");
+	is_string("longer str", dest,
+			"Result of copying a string that is too long.");
+	ok(StringCbCopyA(dest, 0, "test") == STRSAFE_E_INVALID_PARAMETER,
+			"Make sure error is thrown if cchDest is zero.");
 
 	return 0;
 }
--- tests/StringCchCopyA-t.c
+++ tests/StringCchCopyA-t.c
@@ -2,7 +2,20 @@
 #include "tap/basic.h"
 
 int main(void){
-	plan(0);
+	plan(5);
+
+	char dest[11];
+	ok(SUCCEEDED(StringCchCopyA(dest, 11, "test")),
+			"Copy short string.");
+	is_string("test", dest,
+			"Result of copying short string.");
+	ok(StringCchCopyA(dest, 11, "longer string") ==
+			STRSAFE_E_INSUFFICIENT_BUFFER,
+			"Copy a string that is too long.");
+	is_string("longer str", dest,
+			"Result of copying a string that is too long.");
+	ok(StringCchCopyA(dest, 0, "test") == STRSAFE_E_INVALID_PARAMETER,
+			"Make sure error is thrown if cchDest is zero.");
 
 	return 0;
 }

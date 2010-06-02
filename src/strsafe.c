f23f71653892d638ac31f63b9d433d26b1895e92
Jonas Sundberg <jsund@jsund.com>
2010-06-02 17:38:55 +0200

    Added Unicode versions of Cat/Copy/Length.

--- src/strsafe.c
+++ src/strsafe.c
@@ -39,6 +39,31 @@ HRESULT StringCchCatA(
 	return StringCchCopyA(pszDest + length, cchDest - length, pszSrc);
 }
 
+HRESULT StringCchCatW(
+		__inout	LPWSTR pszDest,
+		__in	size_t cchDest,
+		__in	LPCWSTR pszSrc){
+	size_t length;
+	HRESULT result;
+
+	if(cchDest == 0 || cchDest > STRSAFE_MAX_CCH){
+		/* Invalid value for cchDest. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	if(FAILED(StringCchLengthW(pszDest, cchDest, &length))){
+		/* pszDest not null terminated. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	if(cchDest - length < 2){
+		/* pszDest already full. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	return StringCchCopyW(pszDest + length, cchDest - length, pszSrc);
+}
+
 HRESULT StringCchCatNA(
 		__inout	LPSTR pszDest,
 		__in	size_t cchDest,
@@ -63,6 +88,33 @@ HRESULT StringCchCatNA(
 	}
 
 	return StringCchCopyNA(pszDest + length, cchDest - length,
+			pszSrc, cchSrc);
+}
+
+HRESULT StringCchCatNW(
+		__inout	LPWSTR pszDest,
+		__in	size_t cchDest,
+		__in	LPCWSTR pszSrc,
+		__in	size_t cchSrc){
+	size_t length;
+	HRESULT result;
+
+	if(cchDest == 0 || cchDest > STRSAFE_MAX_CCH){
+		/* Invalid value for cchDest. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	if(FAILED(StringCchLengthW(pszDest, cchDest, &length))){
+		/* pszDest not null terminated. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	if(cchDest - length < 2){
+		/* pszDest already full. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	return StringCchCopyNW(pszDest + length, cchDest - length,
 			pszSrc, cchSrc);
 }
 
@@ -97,6 +149,37 @@ HRESULT StringCchCopyA(
 	return result;
 }
 
+HRESULT StringCchCopyW(
+		__out	LPWSTR pszDest,
+		__in	size_t cchDest,
+		__in	LPCWSTR pszSrc){
+	size_t length;
+	HRESULT result;
+
+	if(cchDest == 0 || cchDest > STRSAFE_MAX_CCH){
+		/* Invalid value for cchDest. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	if(FAILED(StringCchLengthW(pszSrc, STRSAFE_MAX_CCH, &length))){
+		/* Should not happen if pszSrc is null terminated. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	if(length >= cchDest){
+		/* pszSrc too long, copy first cchDest - 1 characters. */
+		length = cchDest - 1;
+		result = STRSAFE_E_INSUFFICIENT_BUFFER;
+	}else{
+		result = S_OK;
+	}
+
+	memcpy(pszDest, pszSrc, length * sizeof(wchar_t));
+	pszDest[length] = L'\0';
+
+	return result;
+}
+
 HRESULT StringCchCopyNA(
 		__out	LPSTR pszDest,
 		__in	size_t cchDest,
@@ -126,6 +209,39 @@ HRESULT StringCchCopyNA(
 
 	memcpy(pszDest, pszSrc, length);
 	pszDest[length] = '\0';
+
+	return result;
+}
+
+HRESULT StringCchCopyNW(
+		__out	LPWSTR pszDest,
+		__in	size_t cchDest,
+		__in	LPCWSTR pszSrc,
+		__in	size_t cchSrc){
+	size_t length;
+	HRESULT result;
+
+	if(cchDest == 0 || cchDest > STRSAFE_MAX_CCH){
+		/* Invalid value for cchDest. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	if(FAILED(StringCchLengthW(pszSrc, cchSrc + 1, &length))){
+		/* pszSrc is longer than the maximum length to copy. */
+		/* Copy only first cchSrc characters. */
+		length = cchSrc;
+	}
+
+	if(length >= cchDest){
+		/* pszSrc too long, copy first cchDest - 1 characters. */
+		length = cchDest - 1;
+		result = STRSAFE_E_INSUFFICIENT_BUFFER;
+	}else{
+		result = S_OK;
+	}
+
+	memcpy(pszDest, pszSrc, length * sizeof(wchar_t));
+	pszDest[length] = L'\0';
 
 	return result;
 }
@@ -199,6 +315,28 @@ HRESULT StringCchLengthA(
 	return S_OK;
 }
 
+HRESULT StringCchLengthW(
+		__in	LPCWSTR psz,
+		__in	size_t cchMax,
+		__out	size_t *pcch){
+	if(psz == NULL || cchMax > STRSAFE_MAX_CCH){
+		/* Invalid value for psz or cchMax. */
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	/* This might be a good target for optimization. */
+	for(*pcch = 0; *pcch < cchMax; ){
+		if(psz[*pcch] == L'\0'){
+			break;
+		}
+		(*pcch)++;
+	}
+	/* If *pcch was incremented to cchMax, the first cchMax characters
+	 * did not contain a null termination. */
+	if(*pcch == cchMax) return STRSAFE_E_INVALID_PARAMETER;
+	return S_OK;
+}
+
 /* Since sizeof(char) == 1, we can let Cb functions wrap Cch functions. */
 
 HRESULT StringCbCatA(
@@ -206,6 +344,13 @@ HRESULT StringCbCatA(
 		__in	size_t cbDest,
 		__in	LPCSTR pszSrc){
 	return StringCchCatA(pszDest, cbDest, pszSrc);
+}
+
+HRESULT StringCbCatW(
+		__inout	LPWSTR pszDest,
+		__in	size_t cbDest,
+		__in	LPCWSTR pszSrc){
+	return StringCchCatW(pszDest, cbDest / sizeof(wchar_t), pszSrc);
 }
 
 HRESULT StringCbCatNA(
@@ -216,11 +361,27 @@ HRESULT StringCbCatNA(
 	return StringCchCatNA(pszDest, cbDest, pszSrc, cbSrc);
 }
 
+HRESULT StringCbCatNW(
+		__out	LPWSTR pszDest,
+		__in	size_t cbDest,
+		__in	LPCWSTR pszSrc,
+		__in	size_t cbSrc){
+	return StringCchCatNW(pszDest, cbDest / sizeof(wchar_t),
+			pszSrc, cbSrc / sizeof(wchar_t));
+}
+
 HRESULT StringCbCopyA(
 		__out	LPSTR pszDest,
 		__in	size_t cbDest,
 		__in	LPCSTR pszSrc){
 	return StringCchCopyA(pszDest, cbDest, pszSrc);
+}
+
+HRESULT StringCbCopyW(
+		__out	LPWSTR pszDest,
+		__in	size_t cbDest,
+		__in	LPCWSTR pszSrc){
+	return StringCchCopyW(pszDest, cbDest / sizeof(wchar_t), pszSrc);
 }
 
 HRESULT StringCbCopyNA(
@@ -231,9 +392,28 @@ HRESULT StringCbCopyNA(
 	return StringCchCopyNA(pszDest, cbDest, pszSrc, cbSrc);
 }
 
+HRESULT StringCbCopyNW(
+		__out	LPWSTR pszDest,
+		__in	size_t cbDest,
+		__in	LPCWSTR pszSrc,
+		__in	size_t cbSrc){
+	return StringCchCopyNW(pszDest, cbDest / sizeof(wchar_t),
+			pszSrc, cbSrc / sizeof(wchar_t));
+}
+
 HRESULT StringCbLengthA(
 		__in	LPCSTR psz,
 		__in	size_t cbMax,
 		__out	size_t *pcb){
 	return StringCchLengthA(psz, cbMax, pcb);
+}
+
+HRESULT StringCbLengthW(
+		__in	LPCWSTR psz,
+		__in	size_t cbMax,
+		__out	size_t *pcb){
+	size_t pcch;
+	HRESULT result = StringCchLengthW(psz, cbMax / sizeof(wchar_t), &pcch);
+	*pcb = pcch;
+	return result;
 }

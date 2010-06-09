dba412067c23e0165275888bcb9da79f5d7a540d
Jonas Sundberg <jsund@jsund.com>
2010-06-09 18:57:58 +0200

    Added missing versions of the Gets function.

--- src/strsafe_gets.c
+++ src/strsafe_gets.c
@@ -21,15 +21,18 @@ HRESULT StringCchGetsA(
 		__out	LPSTR pszDest,
 		__in	size_t cchDest){
 	size_t length = 0;
+
 	if(cchDest < 2){
 		return STRSAFE_E_INSUFFICIENT_BUFFER;
 	}
 	if(cchDest > STRSAFE_MAX_CCH){
 		return STRSAFE_E_INVALID_PARAMETER;
 	}
+
 	while(length < cchDest - 1){
 		int c = getchar();
 		if(c == EOF){
+			pszDest[length] = L'\0';
 			return STRSAFE_E_END_OF_FILE;
 		}
 		if((char)c == '\n'){
@@ -39,4 +42,43 @@ HRESULT StringCchGetsA(
 	}
 	pszDest[length] = '\0';
 	return S_OK;
+}
+
+HRESULT StringCchGetsW(
+		__out	LPWSTR pszDest,
+		__in	size_t cchDest){
+	size_t length = 0;
+
+	if(cchDest < 2){
+		return STRSAFE_E_INSUFFICIENT_BUFFER;
+	}
+	if(cchDest > STRSAFE_MAX_CCH){
+		return STRSAFE_E_INVALID_PARAMETER;
+	}
+
+	while(length < cchDest - 1){
+		wint_t c = getwchar(stdin);
+		if(c == WEOF){
+			pszDest[length] = L'\0';
+			return STRSAFE_E_END_OF_FILE;
+		}
+		if((wchar_t)c == L'\n'){
+			break;
+		}
+		pszDest[length++] = (wchar_t)c;
+	}
+	pszDest[length] = L'\0';
+	return S_OK;
+}
+
+HRESULT StringCbGetsA(
+		__out	LPSTR pszDest,
+		__in	size_t cbDest){
+	return StringCchGetsA(pszDest, cbDest);
+}
+
+HRESULT StringCbGetsW(
+		__out	LPWSTR pszDest,
+		__in	size_t cbDest){
+	return StringCchGetsW(pszDest, cbDest / sizeof(wchar_t));
 }

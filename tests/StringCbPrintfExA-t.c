99647d3df8f5d3f92059d9f958f86a0320ac9655
Jonas Sundberg <jsund@jsund.com>
2010-07-16 18:01:42 +0200

    Added tests for all PrintfEx functions.

--- tests/StringCbPrintfExA-t.c
+++ tests/StringCbPrintfExA-t.c
@@ -1,8 +1,135 @@
 #include <strsafe.h>
 #include "tap/basic.h"
 
+void testDestEnd(){
+	char dest[11];
+	char * destEnd;
+
+	diag("Test calculation of destEnd.");
+
+	ok(SUCCEEDED(StringCbPrintfExA(dest, 11, &destEnd, NULL, 0, "Data")),
+			"Test calculation of destEnd "
+			"while printing short string.");
+	is_string("Data", dest,
+			"Result of printing short string.");
+	ok(destEnd == &dest[4],
+			"Value of destEnd after printing short string.");
+
+	ok(SUCCEEDED(StringCbPrintfExA(dest, 11, &destEnd, NULL, 0, "")),
+			"Test calculation of destEnd "
+			"while printing empty string.");
+	is_string("", dest,
+			"Result of printing empty string.");
+	ok(destEnd == &dest[0],
+			"Value of destEnd after printing empty string.");
+
+	ok(StringCbPrintfExA(dest, 11, &destEnd, NULL, 0, "longer %s",
+				"string") ==
+			STRSAFE_E_INSUFFICIENT_BUFFER,
+			"Test calculation of destEnd "
+			"while printing a too long string.");
+	is_string("longer str", dest,
+			"Result of printing a too long string.");
+	ok(destEnd == &dest[10],
+			"Value of destEnd after printing a too long string.");
+}
+
+void testRemaining(){
+	char dest[11];
+	size_t remaining;
+
+	diag("Test calculation of remaining space.");
+
+	ok(SUCCEEDED(StringCbPrintfExA(dest, 11, NULL, &remaining, 0, "STR")),
+			"Test calculation of remaining space "
+			"while printing short string.");
+	is_string("STR", dest,
+			"Result of printing short string.");
+	is_int(8, remaining,
+			"Number of remaining characters after printing short string.");
+
+	ok(SUCCEEDED(StringCbPrintfExA(dest, 11, NULL, &remaining, 0, "")),
+			"Test calculation of remaining space "
+			"while printing empty string.");
+	is_string("", dest,
+			"Result of printing empty string.");
+	is_int(11, remaining,
+			"Number of remaining characters after printing empty string.");
+
+	ok(StringCbPrintfExA(dest, 11, NULL, &remaining, 0, "too long string")
+			== STRSAFE_E_INSUFFICIENT_BUFFER,
+			"Test calculation of remaining space "
+			"while printing a too long string.");
+	is_string("too long s", dest,
+			"Result of printing a too long string.");
+	is_int(1, remaining,
+			"Number of remaining characters after printing "
+			"a too long string.");
+}
+
+void testFlags(){
+	char dest[11];
+
+	diag("Test the STRSAFE_IGNORE_NULLS flag.");
+
+	ok(SUCCEEDED(StringCbPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_IGNORE_NULLS, NULL)),
+			"Test printing a NULL string.");
+	is_string("", dest,
+			"Result of printing a NULL string.");
+
+	diag("Test the STRSAFE_FILL_BEHIND_NULL flag.");
+
+	ok(SUCCEEDED(StringCbPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_FILL_BEHIND_NULL | '@', "testing")),
+			"Test filling with '@' behind null termination.");
+	is_string("testing", dest,
+			"Result of printing and filling behind null termination.");
+	if(memcmp(&dest[8], "@@@", 3)){
+		diag("wanted: \"@@@\"");
+		diag("  seen: \"%c%c%c\"", dest[8], dest[9], dest[10]);
+	}
+	ok(memcmp(&dest[8], "@@@", 3) == 0,
+			"Correct data filled after null termination.");
+
+	diag("Test the STRSAFE_FILL_ON_FAILURE flag.");
+
+	ok(FAILED(StringCbPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_FILL_ON_FAILURE | '@', "too much data")),
+			"Test filling with '@' on failure.");
+	is_string("@@@@@@@@@@", dest,
+			"Result of filling with '@' on failure.");
+
+	diag("Test the STRSAFE_NULL_ON_FAILURE flag.");
+
+	ok(FAILED(StringCbPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_NULL_ON_FAILURE, "Also too much")),
+			"Test nulling string on failure.");
+	is_string("", dest,
+			"Result when nulling string on failure.");
+
+	diag("Test the STRSAFE_NO_TRUNCATION flag.");
+
+	ok(FAILED(StringCbPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_NO_TRUNCATION, "Won't fit in dest")),
+			"Test printing with truncating disabled.");
+	is_string("", dest,
+			"Result after printing with truncating disabled.");
+}
+
 int main(void){
-	skip_all("Not implemented yet");
+	char dest[11];
+	
+	plan(31);
+
+	ok(SUCCEEDED(StringCbPrintfExA(dest, 11, NULL, NULL, 0, "test")),
+			"Print short string without any extended functionality.");
+	is_string("test", dest,
+			"Result of printing short string.");
+
+	testDestEnd();
+	testRemaining();
+	testFlags();
 
 	return 0;
 }
--- tests/StringCchPrintfExA-t.c
+++ tests/StringCchPrintfExA-t.c
@@ -7,12 +7,11 @@ void testDestEnd(){
 
 	diag("Test calculation of destEnd.");
 
-
 	ok(SUCCEEDED(StringCchPrintfExA(dest, 11, &destEnd, NULL, 0, "Data")),
 			"Test calculation of destEnd "
 			"while printing short string.");
 	is_string("Data", dest,
-			"Result of copying short string.");
+			"Result of printing short string.");
 	ok(destEnd == &dest[4],
 			"Value of destEnd after printing short string.");
 
@@ -20,18 +19,19 @@ void testDestEnd(){
 			"Test calculation of destEnd "
 			"while printing empty string.");
 	is_string("", dest,
-			"Result of copying empty string.");
+			"Result of printing empty string.");
 	ok(destEnd == &dest[0],
-			"Value of destEnd after copying empty string.");
+			"Value of destEnd after printing empty string.");
 
-	ok(StringCchCopyExA(dest, 11, "longer string", &destEnd, NULL, 0) ==
+	ok(StringCchPrintfExA(dest, 11, &destEnd, NULL, 0, "longer %s",
+				"string") ==
 			STRSAFE_E_INSUFFICIENT_BUFFER,
 			"Test calculation of destEnd "
-			"while copying a too long string.");
+			"while printing a too long string.");
 	is_string("longer str", dest,
-			"Result of copying a too long string.");
+			"Result of printing a too long string.");
 	ok(destEnd == &dest[10],
-			"Value of destEnd after copying a too long string.");
+			"Value of destEnd after printing a too long string.");
 }
 
 void testRemaining(){
@@ -40,30 +40,30 @@ void testRemaining(){
 
 	diag("Test calculation of remaining space.");
 
-	ok(SUCCEEDED(StringCchCopyExA(dest, 11, "STR", NULL, &remaining, 0)),
+	ok(SUCCEEDED(StringCchPrintfExA(dest, 11, NULL, &remaining, 0, "STR")),
 			"Test calculation of remaining space "
-			"while copying short string.");
+			"while printing short string.");
 	is_string("STR", dest,
-			"Result of copying short string.");
+			"Result of printing short string.");
 	is_int(8, remaining,
-			"Number of remaining characters after copying short string.");
+			"Number of remaining characters after printing short string.");
 
-	ok(SUCCEEDED(StringCchCopyExA(dest, 11, "", NULL, &remaining, 0)),
+	ok(SUCCEEDED(StringCchPrintfExA(dest, 11, NULL, &remaining, 0, "")),
 			"Test calculation of remaining space "
-			"while copying empty string.");
+			"while printing empty string.");
 	is_string("", dest,
-			"Result of copying empty string.");
+			"Result of printing empty string.");
 	is_int(11, remaining,
-			"Number of remaining characters after copying empty string.");
+			"Number of remaining characters after printing empty string.");
 
-	ok(StringCchCopyExA(dest, 11, "too long string", NULL, &remaining, 0) ==
-			STRSAFE_E_INSUFFICIENT_BUFFER,
+	ok(StringCchPrintfExA(dest, 11, NULL, &remaining, 0, "too long string")
+			== STRSAFE_E_INSUFFICIENT_BUFFER,
 			"Test calculation of remaining space "
-			"while copying a too long string.");
+			"while printing a too long string.");
 	is_string("too long s", dest,
-			"Result of copying a too long string.");
+			"Result of printing a too long string.");
 	is_int(1, remaining,
-			"Number of remaining characters after copying "
+			"Number of remaining characters after printing "
 			"a too long string.");
 }
 
@@ -72,19 +72,19 @@ void testFlags(){
 
 	diag("Test the STRSAFE_IGNORE_NULLS flag.");
 
-	ok(SUCCEEDED(StringCchCopyExA(dest, 11, NULL, NULL, NULL,
-					STRSAFE_IGNORE_NULLS)),
-			"Test copying a NULL string.");
+	ok(SUCCEEDED(StringCchPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_IGNORE_NULLS, NULL)),
+			"Test printing a NULL string.");
 	is_string("", dest,
-			"Result of copying a NULL string.");
+			"Result of printing a NULL string.");
 
 	diag("Test the STRSAFE_FILL_BEHIND_NULL flag.");
 
-	ok(SUCCEEDED(StringCchCopyExA(dest, 11, "testing", NULL, NULL,
-					STRSAFE_FILL_BEHIND_NULL | '@')),
+	ok(SUCCEEDED(StringCchPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_FILL_BEHIND_NULL | '@', "testing")),
 			"Test filling with '@' behind null termination.");
 	is_string("testing", dest,
-			"Result of copying and filling behind null termination.");
+			"Result of printing and filling behind null termination.");
 	if(memcmp(&dest[8], "@@@", 3)){
 		diag("wanted: \"@@@\"");
 		diag("  seen: \"%c%c%c\"", dest[8], dest[9], dest[10]);
@@ -94,27 +94,27 @@ void testFlags(){
 
 	diag("Test the STRSAFE_FILL_ON_FAILURE flag.");
 
-	ok(FAILED(StringCchCopyExA(dest, 11, "too much data", NULL, NULL,
-					STRSAFE_FILL_ON_FAILURE | '@')),
+	ok(FAILED(StringCchPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_FILL_ON_FAILURE | '@', "too much data")),
 			"Test filling with '@' on failure.");
 	is_string("@@@@@@@@@@", dest,
 			"Result of filling with '@' on failure.");
 
 	diag("Test the STRSAFE_NULL_ON_FAILURE flag.");
 
-	ok(FAILED(StringCchCopyExA(dest, 11, "Also too much", NULL, NULL,
-					STRSAFE_NULL_ON_FAILURE)),
+	ok(FAILED(StringCchPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_NULL_ON_FAILURE, "Also too much")),
 			"Test nulling string on failure.");
 	is_string("", dest,
 			"Result when nulling string on failure.");
 
 	diag("Test the STRSAFE_NO_TRUNCATION flag.");
 
-	ok(FAILED(StringCchCopyExA(dest, 11, "Won't fit in dest", NULL, NULL,
-					STRSAFE_NO_TRUNCATION)),
-			"Test copying with truncating disabled.");
+	ok(FAILED(StringCchPrintfExA(dest, 11, NULL, NULL,
+					STRSAFE_NO_TRUNCATION, "Won't fit in dest")),
+			"Test printing with truncating disabled.");
 	is_string("", dest,
-			"Result after copying with truncating disabled.");
+			"Result after printing with truncating disabled.");
 }
 
 int main(void){
@@ -122,10 +122,10 @@ int main(void){
 	
 	plan(31);
 
-	ok(SUCCEEDED(StringCchCopyExA(dest, 11, "test", NULL, NULL, 0)),
-			"Copy short string without any extended functionality.");
+	ok(SUCCEEDED(StringCchPrintfExA(dest, 11, NULL, NULL, 0, "test")),
+			"Print short string without any extended functionality.");
 	is_string("test", dest,
-			"Result of copying short string.");
+			"Result of printing short string.");
 
 	testDestEnd();
 	testRemaining();
